d33d085906013cf73eefe1f6fb38272aebf8dd4d
Konstantin <constantin.l.o.p@gmail.com>
2020-05-11 17:57:07 +0700

    Initial Commit

---
+++ safepassword/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  safepassword
+//
+//  Created by Константин Лопаткин on 11.05.2020.
+//  Copyright © 2020 Konstantin Lopatkin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
